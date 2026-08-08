934fd0b323104f435f351d88bc4b0bcb458152ff
alexstoliar <ostol@softserveinc.com>
2026-08-08 17:55:26 +0300

    jenkinsfile edited

--- playwright.config.js
+++ playwright.config.js
@@ -1,3 +1,17 @@
-// @ts-check
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+    testDir: './e2e',
+
+    use: {
+        baseURL: 'http://127.0.0.1:3000',
+    },
+
+    webServer: {
+        command: 'npx serve -s build -l 3000',
+        url: 'http://127.0.0.1:3000',
+        reuseExistingServer: true,
+    },
+});// @ts-check
 
 
