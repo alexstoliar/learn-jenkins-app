f7a0af534c848082678392dbac0f7acc6393c6db
alexstoliar <ostol@softserveinc.com>
2026-08-11 18:02:53 +0300

    jenkinsfile edited

--- playwright.config.js
+++ playwright.config.js
@@ -4,14 +4,16 @@ export default defineConfig({
     testDir: './e2e',
 
     use: {
-        baseURL: process.env.CI_ENVIRONMENT_URL || 'http://127.0.0.1:3000',
+        baseURL:
+            process.env.CI_ENVIRONMENT_URL ||
+            'http://127.0.0.1:3000',
     },
 
-    webServer: {
-        command: 'npx serve -s build -l 3000',
-        url: 'http://127.0.0.1:3000',
-        reuseExistingServer: true,
-    },
-});// @ts-check
-
-
+    webServer: process.env.CI_ENVIRONMENT_URL
+        ? undefined
+        : {
+            command: 'npx serve -s build -l 3000',
+            url: 'http://127.0.0.1:3000',
+            reuseExistingServer: true,
+        },
+});
